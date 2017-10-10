00a31efd9502afc9a7a6ed76d9422cbf26a66492
maximillianfx <maximillianfx@gmail.com>
2017-10-10 10:42:46 -0300

    Ajuste de modularização

--- main.py
+++ main.py
@@ -7,32 +7,21 @@ from ClassInfo import *
 
 def main():
 
+	#Limpeza de terminal e exibição de informações do projeto
 	ClasseInformativo = ClassInfo()
 	ClasseInformativo.limparTela()
 	ClasseInformativo.informativoPrograma()
 
+	#Criação das listas de processos e linhas do arquivo .txt de entrada
 	linhasArquivo = []
 	vetor_processos = []
 
+	#Leitura das linhas do arquivo .txt de entrada com os processos
 	linhasArquivo = lendoArquivoProcesses()
 
+	vetor_processos = montaProcesses(linhasArquivo)
 
-	for linha in linhasArquivo:
-		atri_Processo = linha.split(",")
-		print(atri_Processo)
-
-		processo_temporario = ClassProcesso(int(atri_Processo[0]),
-							int(atri_Processo[1]), int(atri_Processo[2]),
-							int(atri_Processo[3]), int(atri_Processo[4]),
-							int(atri_Processo[5]), int(atri_Processo[6]),
-							int(atri_Processo[7]))
-
-
-		vetor_processos.append(processo_temporario)
-
-	vetor_processos[0].imprimirValoresProcesso()
-	print("-----------------------------------------------")
-	vetor_processos[1].imprimirValoresProcesso()
+	imprimeProcessos(vetor_processos)
 
 
 
@@ -71,9 +60,30 @@ def envelhecimento(fila_processos):
 	return fila_processos
 
 
+def montaProcesses (linhasArquivoProcesses):
+
+	vetor_auxiliar = []
+
+	for linha in linhasArquivoProcesses:
+		atri_Processo = linha.split(",")
+
+		processo_temporario = ClassProcesso(int(atri_Processo[0]),
+							int(atri_Processo[1]), int(atri_Processo[2]),
+							int(atri_Processo[3]), int(atri_Processo[4]),
+							int(atri_Processo[5]), int(atri_Processo[6]),
+							int(atri_Processo[7]))
+
+
+		vetor_auxiliar.append(processo_temporario)
+
+	return vetor_auxiliar
+
+
+def imprimeProcessos(vetorProcessos):
+	for processo in vetorProcessos:
+		processo.imprimirValoresProcesso()
+		print("-----------------------------------------------")
+
 
 if __name__ == "__main__":
 	main()
-
-
-
